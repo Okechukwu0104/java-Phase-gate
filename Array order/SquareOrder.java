e55b1fb1dfdec6c371c3f2927d42ea3b2696e0f6
Okechukwu Peter <okechukwupeter10@gmail.com>
2024-10-03 16:49:56 +0100

    phase 3

--- Array order/SquareOrder.java
+++ Array order/SquareOrder.java
@@ -7,6 +7,7 @@ public class SquareOrder{
 
 	int [] array1 = {2,1,4,3,5,9};
 	int [] array2 = new int [array1.length];
+	int [] array3 = new int [array1.length];
 
 	for(int count = 0; count < array1.length; count++){
 	    array2[count] = array1[count] * array1[count];
@@ -15,8 +16,24 @@ public class SquareOrder{
 	
 	System.out.println("original: "+Arrays.toString(array1));
 
-	Arrays.sort(array2);
-	System.out.print("Squared & arranged: "+Arrays.toString(array2));
+	for(int count = 0; count < array2.length; count++){
+		int digit = array2[count];
+		int temp = 0;
+
+	    for(int counts = 0; counts < 24; counts++){
+		if(digit > array2[count])continue;
+		else{
+		   temp = array2[count];
+		   
+		array3[count] = temp; 
+		   
+		}
+		
+
+	    }
+	}
+
+	System.out.print("Squared & arranged: "+Arrays.toString(array3));
 	
 
    }
